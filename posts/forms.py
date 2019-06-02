c333a563bf707d0d31b2837f27da66ff815a2e44
Vladanak <vladmyr89@gmail.com>
2019-06-02 20:03:53 +0300

    New_23

--- posts/forms.py
+++ posts/forms.py
@@ -1,17 +1,27 @@
 from django import forms
+from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
 
 
 class PostForm(forms.Form):
     text = forms.CharField(widget=forms.TextInput(attrs={
         'size': 30,
         'class': 'form-control',
-        'placeholder': 'Введите Ф.И.О. сотрудника'
-    }), max_length=100, min_length=3)
+        'placeholder': 'Введите Ф.И.О. сотрудника без цифр и символов'
+    }), max_length=100, min_length=3, validators=[RegexValidator(
+            regex='^[A-zА-яЁё]*$',
+            message=_('Username must be Alphanumeric'),
+            code='invalid_username'
+        )])
     specialty = forms.CharField(widget=forms.TextInput(attrs={
         'size': 30,
         'class': 'form-control',
-        'placeholder': 'Введите специальность',
-    }), max_length=30, min_length=1)
+        'placeholder': 'Введите специальность без цифр и символов',
+    }), max_length=30, min_length=1, validators=[RegexValidator(
+            regex='^[A-zА-яЁё]*$',
+            message='Username must be Alphanumeric',
+            code='invalid_username'
+        )])
     plan = forms.IntegerField(widget=forms.NumberInput(attrs={
         'size': 30,
         'class': 'form-control',
@@ -21,17 +31,17 @@ class PostForm(forms.Form):
         'size': 30,
         'class': 'form-control',
         'placeholder': 'Введите фактической результат',
-    }), max_value=999999, min_value=0)
+    }), max_value=999999, min_value=1)
     plan_clock = forms.IntegerField(widget=forms.NumberInput(attrs={
         'size': 30,
         'class': 'form-control',
         'placeholder': 'Введите план времени',
-    }), max_value=99999, min_value=0)
+    }), max_value=99999, min_value=1)
     plan_clock_rez = forms.IntegerField(widget=forms.NumberInput(attrs={
         'size': 30,
         'class': 'form-control',
         'placeholder': 'Введите фактическое отработанное время',
-    }), max_value=99999, min_value=0)
+    }), max_value=99999, min_value=1)
     plan_defect = forms.IntegerField(widget=forms.NumberInput(attrs={
         'size': 30,
         'class': 'form-control',
@@ -43,4 +53,8 @@ class SearchForm(forms.Form):
     q = forms.CharField(widget=forms.TextInput(attrs={
         'size': 30,
         'class': 'form-control search-query',
-    }))
+    }), validators=[RegexValidator(
+            regex='^[A-zА-яЁё]*$',
+            message=_('fIELD must be Alphanumeric'),
+            code='invalid_username'
+        )])
